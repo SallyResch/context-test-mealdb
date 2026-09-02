ed5ea856716c3f7a0767d5af2b1bdff48dd61bb4
Sally Resch <sallyresch27@gmail.com>
2026-09-02 08:19:07 +0200

    asd

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
-import Navigation from "@/components/Navigation";
-import Login from "@/components/Login";
 import Footer from "@/components/Footer";
 import { UserProvider } from "@/contexts/userContext";
 import LoginWrapper from "@/components/LoginWrapper";
