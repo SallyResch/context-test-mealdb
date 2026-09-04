909d926f3e2a9aa670f47e9a38085be8dcf7891e
Sally Resch <sallyresch27@gmail.com>
2026-09-04 08:48:56 +0200

    dynamic recipe page

--- app/recipes/[id]/page.tsx
+++ app/recipes/[id]/page.tsx
@@ -9,6 +9,13 @@ const Recipe = async({params}:{params:{id:string}}) => {
             const data = await response.json()
             recipe = data.meals[0]
             console.log(recipe)
+            const keys = Object.keys(recipe!).filter(key => key.includes("strIngredient"))
+            console.log(keys)
+            const keysValue = keys.filter((key:string) => recipe[key] !== "" && recipe[key] !== null)
+            console.log(keysValue)
+            const ingredients = keysValue.map((key, index) => `${recipe[key]} - ${recipe[`strMeasure${index + 1}`]}`)
+            console.log(ingredients)
+            recipe.ingredients = ingredients;
         } catch (error) {
             console.log(error)
         }
@@ -16,7 +23,18 @@ const Recipe = async({params}:{params:{id:string}}) => {
     return (
         <div>
             <h2>{id}</h2>
-            {recipe && recipe.strMeal}
+            {recipe && <div>
+                <h2>{recipe.strMeal}</h2>
+                <img src={recipe.strMealThumb} alt={recipe.strMeal}  width={300}/>
+                <p>Category: {recipe.strCategory}</p>
+                <p>Country: {recipe.strCountry}</p>
+                <p>Instructions: {recipe.strInstructions}</p>
+                <div>
+                    <h3>Ingredients</h3>
+                    {recipe.ingredients}
+                </div>
+                
+            </div>}
         </div>
         
     )
