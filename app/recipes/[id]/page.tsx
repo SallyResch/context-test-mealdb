0566bbc5834a621d7119a569b3dba5eb6b138060
Sally Resch <sallyresch27@gmail.com>
2026-09-04 08:50:49 +0200

    removed console logs

--- app/recipes/[id]/page.tsx
+++ app/recipes/[id]/page.tsx
@@ -8,13 +8,9 @@ const Recipe = async({params}:{params:{id:string}}) => {
             const response = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}lookup.php?i=${id}`)
             const data = await response.json()
             recipe = data.meals[0]
-            console.log(recipe)
             const keys = Object.keys(recipe!).filter(key => key.includes("strIngredient"))
-            console.log(keys)
             const keysValue = keys.filter((key:string) => recipe[key] !== "" && recipe[key] !== null)
-            console.log(keysValue)
             const ingredients = keysValue.map((key, index) => `${recipe[key]} - ${recipe[`strMeasure${index + 1}`]}`)
-            console.log(ingredients)
             recipe.ingredients = ingredients;
         } catch (error) {
             console.log(error)
@@ -22,7 +18,6 @@ const Recipe = async({params}:{params:{id:string}}) => {
 
     return (
         <div>
-            <h2>{id}</h2>
             {recipe && <div>
                 <h2>{recipe.strMeal}</h2>
                 <img src={recipe.strMealThumb} alt={recipe.strMeal}  width={300}/>
