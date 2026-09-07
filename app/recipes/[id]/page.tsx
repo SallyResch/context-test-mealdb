9fc4164a811b530581749cd5b5a164b6653e93d9
SallyResch <sallyresch27@gmail.com>
2026-09-07 11:49:13 +0200

    changed the way the ingredients array is created

--- app/recipes/[id]/page.tsx
+++ app/recipes/[id]/page.tsx
@@ -3,42 +3,50 @@ import { RecipesDetail } from "@/types/recipeDetails";
 import { RecipesType } from "@/types/recipes";
 
 
-const Recipe = async({params}:{params:{id:string}}) => {
-    const {id} = await params
-    let recipe:RecipesDetail | undefined;
-        try {
-            const response = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}lookup.php?i=${id}`)
-            const data = await response.json()
-            recipe = data.meals[0]
-            const keys = Object.keys(recipe!).filter(key => key.includes("strIngredient"))
-            const keysValue = keys.filter((key:string) => recipe![key as keyof RecipesDetail] !== "" && recipe![key as keyof RecipesDetail] !== null)
-            const ingredients = keysValue.map((key, index) => `${recipe![key as keyof RecipesDetail]} - ${recipe[`strMeasure${index + 1}`]}`)
-            recipe!.ingredients = ingredients;
-        } catch (error) {
-            console.log(error)
+const Recipe = async ({ params }: { params: { id: string } }) => {
+    const { id } = await params
+    let recipe: RecipesDetail | undefined;
+    try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}lookup.php?i=${id}`)
+        const data = await response.json()
+
+        recipe = data.meals[0]
+        console.log(recipe)
+        const ingredients: string[] = []
+        for (let i = 1; i <= 20; i++) {
+            const ingredient = recipe![`strIngredient${i}` as keyof RecipesDetail];
+            const measure = recipe![`strMeasure${i}` as keyof RecipesDetail];
+
+            if (ingredient) {
+                ingredients.push(`${ingredient} - ${measure}`);
+            }
         }
+        recipe!.ingredients = ingredients
+    } catch (error) {
+        console.log(error)
+    }
 
     return (
         <div>
-            {recipe && 
-            <div className="flex flex-col items-center">
-                <div className="flex flex-row max-w-[300px]">
-                <h2 className="text-4xl font-semibold">{recipe.strMeal}</h2>
-                <LikeButton idMeal={recipe.idMeal} strMeal={recipe.strMeal} strMealThumb={recipe.strMealThumb}/>
-                </div>
-                <img src={recipe.strMealThumb} alt={recipe.strMeal}  width={300} className="rounded border border-amber-500"/>
-                <p>Category: {recipe.strCategory}</p>
-                <p>Country: {recipe.strCountry}</p>
-                <p>Instructions: {recipe.strInstructions}</p>
-                <div>
-                    <h3>Ingredients</h3>
-                    <div className=" max-w-md">
-                    {recipe.ingredients}
+            {recipe &&
+                <div className="flex flex-col items-center">
+                    <div className="flex flex-row max-w-[300px]">
+                        <h2 className="text-4xl font-semibold">{recipe.strMeal}</h2>
+                        <LikeButton idMeal={recipe.idMeal} strMeal={recipe.strMeal} strMealThumb={recipe.strMealThumb} />
                     </div>
-                </div>
-            </div>}
+                    <img src={recipe.strMealThumb} alt={recipe.strMeal} width={300} className="rounded border border-amber-500" />
+                    <p>Category: {recipe.strCategory}</p>
+                    <p>Country: {recipe.strCountry}</p>
+                    <p>Instructions: {recipe.strInstructions}</p>
+                    <div>
+                        <h3>Ingredients</h3>
+                        <div className=" max-w-md">
+                            {recipe.ingredients}
+                        </div>
+                    </div>
+                </div>}
         </div>
-        
+
     )
 }
 
