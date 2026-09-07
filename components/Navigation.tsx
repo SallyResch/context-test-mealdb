58c61dc1c1984586f7197a611c5cda0381aebb76
SallyResch <sallyresch27@gmail.com>
2026-09-07 10:33:30 +0200

    added handleMenu onCLick function to hamburger menu button

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,12 +1,30 @@
 "use client"
 import Link from "next/link"
 import Logout from "./Logout"
-import { MenuIcon } from "lucide-react"
+import { MenuIcon, XIcon } from "lucide-react"
+import { useState } from "react"
 
 const Navigation = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+
   return (
-    <div>
-      <MenuIcon />
+    <nav>
+      <div>
+        <button onClick={handleMenu}>
+          {isMenuOpen ? <XIcon /> : <MenuIcon />}
+        </button>
+
+      </div>
+      {isMenuOpen && (
+        <div className="flex flex-row justify-evenly m-auto">
+          <Link className="text-fuchsia-950 hover:bg-fuchsia-950 hover:text-amber-500 rounded p-2" href={"/"}>Home</Link>
+          <Link className="text-fuchsia-950 hover:bg-fuchsia-950 hover:text-amber-500 rounded p-2" href={"/about"}>About</Link>
+          <Link className="text-fuchsia-950 hover:bg-fuchsia-950 hover:text-amber-500 rounded p-2" href={"/categories"}>Categories</Link>
+          <Link className="text-fuchsia-950 hover:bg-fuchsia-950 hover:text-amber-500 rounded p-2" href={"/profile"}>Profile</Link>
+          <Logout />
+        </div>
+      )}
       <div className="bg-amber-500 font-semibold p-2 hidden md:block">
         <div className="flex flex-row justify-evenly m-auto">
           <Link className="text-fuchsia-950 hover:bg-fuchsia-950 hover:text-amber-500 rounded p-2" href={"/"}>Home</Link>
@@ -16,7 +34,7 @@ const Navigation = () => {
           <Logout />
         </div>
       </div>
-    </div>
+    </nav>
   )
 }
 
