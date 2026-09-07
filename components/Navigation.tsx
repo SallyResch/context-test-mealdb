df01a0c90d5682922fc976dfcc6a8850e4973b8e
SallyResch <sallyresch27@gmail.com>
2026-09-07 10:27:50 +0200

    added responsive navigation restrictions

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,17 +1,20 @@
+"use client"
 import Link from "next/link"
 import Logout from "./Logout"
+import { MenuIcon } from "lucide-react"
 
-type Props = {}
-
-const Navigation = (props: Props) => {
+const Navigation = () => {
   return (
-    <div className="bg-amber-500 font-semibold p-2">
-      <div className="max-w-md flex flex-row justify-evenly m-auto">
-        <Link className="text-fuchsia-950 hover:bg-fuchsia-950 hover:text-amber-500 rounded p-2" href={"/"}>Home</Link>
-        <Link className="text-fuchsia-950 hover:bg-fuchsia-950 hover:text-amber-500 rounded p-2" href={"/about"}>About</Link>
-        <Link className="text-fuchsia-950 hover:bg-fuchsia-950 hover:text-amber-500 rounded p-2" href={"/categories"}>Categories</Link>
-        <Link className="text-fuchsia-950 hover:bg-fuchsia-950 hover:text-amber-500 rounded p-2" href={"/profile"}>Profile</Link>
-        <Logout />
+    <div>
+      <MenuIcon />
+      <div className="bg-amber-500 font-semibold p-2 hidden md:block">
+        <div className="flex flex-row justify-evenly m-auto">
+          <Link className="text-fuchsia-950 hover:bg-fuchsia-950 hover:text-amber-500 rounded p-2" href={"/"}>Home</Link>
+          <Link className="text-fuchsia-950 hover:bg-fuchsia-950 hover:text-amber-500 rounded p-2" href={"/about"}>About</Link>
+          <Link className="text-fuchsia-950 hover:bg-fuchsia-950 hover:text-amber-500 rounded p-2" href={"/categories"}>Categories</Link>
+          <Link className="text-fuchsia-950 hover:bg-fuchsia-950 hover:text-amber-500 rounded p-2" href={"/profile"}>Profile</Link>
+          <Logout />
+        </div>
       </div>
     </div>
   )
