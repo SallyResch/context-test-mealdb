59b1d80cb31d14dfe5a53187dbed1b726d5c515b
SallyResch <sallyresch27@gmail.com>
2026-09-10 11:43:18 +0200

    added font to subtitle in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,11 @@
 import { Great_Vibes } from "next/font/google"
-
+import { Caveat } from "next/font/google"
 const greatVibes = Great_Vibes({
+  subsets: ["latin"],
+  weight: "400",
+})
+
+const caveat = Caveat({
   subsets: ["latin"],
   weight: "400",
 })
@@ -9,7 +14,7 @@ const Header = () => {
   return (
     <div className="bg-fuchsia-950 p-2 text-center">
       <h1 className={`${greatVibes.className} text-amber-500 text-6xl mt-4`}>Sally´s</h1>
-      <h2 className={`text-amber-500 text-lg mt-2 mb-1`}>Coziest corner in town</h2>
+      <h2 className={`${caveat.className} text-amber-500 text-3xl mt-2 mb-1`}>Coziest corner in town</h2>
     </div>
   )
 }
