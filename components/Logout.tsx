796c82e600891161dab99cefc66f3592908c6894
SallyResch <sallyresch27@gmail.com>
2026-09-13 13:18:49 +0200

    fixed logout button again in mobile view of navigation

--- components/Logout.tsx
+++ components/Logout.tsx
@@ -1,20 +1,18 @@
 "use client"
 import { useUserContext } from "@/contexts/userContext"
-import { users } from "@/data/users"
 import { UserContextType } from "@/types/context"
 import { LogOutIcon } from "lucide-react"
 import Link from "next/link"
 
 const Logout = () => {
     const { setUser } = useUserContext() as UserContextType
-
     const handleLogout = () => {
         setUser(null)
     }
 
     return (
         <Link href={"/"} onClick={handleLogout} className="flex md:justify-center items-center text-fuchsia-950 hover:cursor-pointer hover:bg-amber-300 rounded hover:shadow-2xl ">
-            <p className="mr-2 ml-2">| Logout</p>
+            <p className="md:mr-2 md:ml-2 ">| Logout</p>
             <LogOutIcon />
         </Link>
     )
