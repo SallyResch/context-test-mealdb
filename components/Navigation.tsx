a73e7baaffc62164f3b742c44c85dbd9f5d87283
SallyResch <sallyresch27@gmail.com>
2026-09-13 13:38:14 +0200

    made an overlay to stop the scrolling

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -2,7 +2,7 @@
 import Link from "next/link"
 import Logout from "./Logout"
 import { MenuIcon, XIcon } from "lucide-react"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const Navigation = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -22,6 +22,17 @@ const Navigation = () => {
     { name: "Profile", href: "/profile" }
   ]
 
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+    return () => {
+      document.body.style.overflow = "";
+    }
+  }, [isMenuOpen])
+
   return (
     <nav>
       <div className="md:hidden bg-amber-500 text-fuchsia-950">
@@ -33,20 +44,23 @@ const Navigation = () => {
         </div>
 
         {isMenuOpen && (
-          <div className="fixed top-35 bottom-0 left-[65%] right-0 z-50 bg-amber-500 shadow-2xl">
-            <div className="flex justify-end p-2">
-              <button onClick={handleMenu}>
-                <XIcon />
-              </button>
-            </div>
-            <div className="flex flex-col h-[calc(100vh-48px)] font-semibold text-lg pl-4">
-              {navItems.map((item) => (
-                <Link key={item.href} onClick={handleNavClick} className="text-fuchsia-950 hover:bg-fuchsia-950 hover:text-amber-500 rounded p-2" href={item.href}>{item.name}</Link>
-              ))}
-              <Logout />
+          <div className="fixed top-35 bg-black/50 z-50 left-0 bottom-0 right-0">
+            <div className="fixed top-35 bottom-0 left-[65%] right-0 z-50 bg-amber-500 shadow-2xl">
+              <div className="flex justify-end p-2">
+                <button onClick={handleMenu}>
+                  <XIcon />
+                </button>
+              </div>
+              <div className="flex flex-col h-[calc(100vh-48px)] font-semibold text-lg pl-4">
+                {navItems.map((item) => (
+                  <Link key={item.href} onClick={handleNavClick} className="text-fuchsia-950 hover:bg-fuchsia-950 hover:text-amber-500 rounded p-2" href={item.href}>{item.name}</Link>
+                ))}
+                <Logout />
+              </div>
             </div>
           </div>
         )}
+
       </div>
 
       <div className="bg-amber-500 font-semibold p-2 hidden md:block">
