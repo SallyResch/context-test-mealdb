64464903c593b88d019c74e106264d9f81c090c5
SallyResch <sallyresch27@gmail.com>
2026-09-14 13:49:00 +0200

    last styling fixes

--- app/recipes/[id]/page.tsx
+++ app/recipes/[id]/page.tsx
@@ -25,20 +25,29 @@ const Recipe = async ({ params }: { params: { id: string } }) => {
     }
 
     return (
-        <div>
+        <div className="my-10">
             {recipe &&
-                <div className="flex flex-col items-center max-w-md m-auto">
-                    <div className="flex flex-row">
+                <div className="flex flex-col max-w-md m-auto">
+                    <div className="flex flex-row justify-center items-center mb-5">
                         <h2 className="text-4xl font-semibold">{recipe.strMeal}</h2>
                         <LikeButton idMeal={recipe.idMeal} strMeal={recipe.strMeal} strMealThumb={recipe.strMealThumb} />
                     </div>
-                    <img src={recipe.strMealThumb} alt={recipe.strMeal} width={300} className="rounded border border-amber-500" />
-                    <p>Category: {recipe.strCategory}</p>
-                    <p>Country: {recipe.strCountry}</p>
-                    <p>Instructions: {recipe.strInstructions}</p>
-                    <div>
-                        <h3>Ingredients</h3>
-                        <div className=" max-w-md">
+                    <img src={recipe.strMealThumb} alt={recipe.strMeal} width={300} className="rounded border border-amber-500 self-center" />
+                    <div className="flex flex-row mt-5 p-2">
+                        <p className="font-semibold">Category:</p>
+                        <p className="ml-2">{recipe.strCategory}</p>
+                    </div>
+                    <div className="flex flex-row mt-5 p-2">
+                        <p className="font-semibold">Country:</p>
+                        <p className="ml-2">{recipe.strCountry}</p>
+                    </div>
+                    <div className="mt-5 p-2">
+                        <p className="font-semibold">Instructions: </p>
+                        <p>{recipe.strInstructions}</p>
+                    </div>
+                    <div className="mt-5 p-2">
+                        <h3 className="font-semibold">Ingredients</h3>
+                        <div className="max-w-md">
                             {recipe.ingredients}
                         </div>
                     </div>
